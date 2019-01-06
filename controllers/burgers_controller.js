5d39a4f58971e451aafe13fa7a468e284dc0ad85
Isabel <iarcones@gmail.com>
2019-01-05 17:13:47 -0800

    working on reload

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -8,12 +8,9 @@ var db = require("../models");
 
 
 router.get("/", function (req, res) {
-    console.log("req.cookies");
-    console.log(req.cookies);
 
     var customerName = req.cookies.customername;
     var customerId = req.cookies.customerid;
-    console.log("customerName: ", customerName);
 
     db.Burger.findAll({
         order: [
@@ -42,7 +39,6 @@ router.get("/", function (req, res) {
 });
 
 router.post("/api/burgers", function (req, res) {
-    console.log("creating: ", req.body.name);
     db.Burger.create({
         burger_name: req.body.name,
     })
@@ -56,37 +52,36 @@ router.put("/api/devoured/:id/:customerId", function (req, res) {
     client = req.params.customerId;
 
     db.Burger.increment('burger_counter', { where: { id: req.params.id } }).then(function (data) {
-        console.log("data when update the counter on burger")
-        console.log(data[0][1]);
-        console.log("data before creating: ", burger, client);
-        db.Customerburger.increment('counter', { where: { BurgerId: burger, CustomerId: client } }).then(function (data) {
-            console.log("inside function increment customerburger");
-            console.log("data: ", data);
-            console.log(data[0][1]);
-            /// if rec doesn't exist create and counter = 1;
-            if (data[0][1] === (0)) {
-                console.log("data for creating: ", burger, client);
-                db.Customerburger.create(
-                    {
-                        BurgerId: burger,
-                        CustomerId: client
-                    })
-                    .then(function (dbcustomerburger) {
-                        console.log(dbcustomerburger)
-                        //res.json(dbcustomerburger);
-                    });
-            }
+        console.log("client: ", client)
+        if (client !== 'null') {
+            console.log("client in the if: ", client)
+            db.Customerburger.increment('counter', { where: { BurgerId: burger, CustomerId: client } }).then(function (data) {
+             
+                /// if rec doesn't exist create and counter = 1;
+                if (data[0][1] === (0)) {
+                    db.Customerburger.create(
+                        {
+                            BurgerId: burger,
+                            CustomerId: client
+                        })
+                        .then(function (dbcustomerburger) {
+                            console.log(dbcustomerburger)
+                            //res.json(dbcustomerburger);
+                        });
+                }
 
-        });
+            });
+        }
     });
+    res.json(data);
 });
 
 router.post("/api/customers", function (req, res) {
-    console.log("creating: ", req.body.name);
+  
     db.Customer.create({
         customer_name: req.body.name,
     }).then(function (dbCustomer) {
-        console.log(dbCustomer);
+       
         res.json(dbCustomer);
     });
 });
